abc14ed6dfdab1f30c3fd45204655f2920f10cf7
Trey Hoover <treyhoover@gmail.com>
2024-11-13 21:01:12 -0500

    Add POC

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,67 +1,71 @@
 "use client";
 
-import { useEffect, useState, ChangeEvent, FormEvent } from "react";
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
 import { Button } from "@repo/ui/button";
+import { log } from "@repo/logger";
 
 const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3001";
 
 export default function Web() {
-  const [name, setName] = useState<string>("");
-  const [response, setResponse] = useState<{ message: string } | null>(null);
-  const [error, setError] = useState<string | undefined>();
+	const [name, setName] = useState<string>("");
+	const [response, setResponse] = useState<{ message: string } | null>(null);
+	const [error, setError] = useState<string | undefined>();
 
-  useEffect(() => {
-    setResponse(null);
-    setError(undefined);
-  }, [name]);
+	useEffect(() => {
+		setResponse(null);
+		setError(undefined);
+	}, []);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setName(e.target.value);
+	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+		log(`name changed: ${e.target.value}`);
 
-  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+		setName(e.target.value);
+	};
 
-    try {
-      const result = await fetch(`${API_HOST}/message/${name}`);
-      const response = await result.json();
-      setResponse(response);
-    } catch (err) {
-      console.error(err);
-      setError("Unable to fetch response");
-    }
-  };
+	const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-  const onReset = () => {
-    setName("");
-  };
+		try {
+			const result = await fetch(`${API_HOST}/message/${name}`);
+			const response = await result.json();
+			setResponse(response);
+		} catch (err) {
+			console.error(err);
+			setError("Unable to fetch response");
+		}
+	};
 
-  return (
-    <div>
-      <h1>Web</h1>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="name">Name </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={onChange}
-        ></input>
-        <Button type="submit">Submit</Button>
-      </form>
-      {error && (
-        <div>
-          <h3>Error</h3>
-          <p>{error}</p>
-        </div>
-      )}
-      {response && (
-        <div>
-          <h3>Greeting</h3>
-          <p>{response.message}</p>
-          <Button onClick={onReset}>Reset</Button>
-        </div>
-      )}
-    </div>
-  );
+	const onReset = () => {
+		setName("");
+	};
+
+	return (
+		<div>
+			<h1>Web</h1>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="name">Name </label>
+				<input
+					type="text"
+					name="name"
+					id="name"
+					value={name}
+					onChange={onChange}
+				/>
+				<Button type="submit">Submit</Button>
+			</form>
+			{error && (
+				<div>
+					<h3>Error</h3>
+					<p>{error}</p>
+				</div>
+			)}
+			{response && (
+				<div>
+					<h3>Greeting</h3>
+					<p>{response.message}</p>
+					<Button onClick={onReset}>Reset</Button>
+				</div>
+			)}
+		</div>
+	);
 }
